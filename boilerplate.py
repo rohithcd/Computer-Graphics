41c805c9624f9e39ba2a0853fcf76fb180d578c4
ROHITH C DINESAN <56485934+rohithmaddz@users.noreply.github.com>
2021-10-02 01:06:26 +0530

    added boilerplate file

---
+++ boilerplate.py
@@ -0,0 +1,31 @@
+# Importing everything from libraries GL, GLU & GLUT
+from OpenGL.GL import * 
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+
+
+def init():
+    glClearColor(0, 1, 0, 1) #Setting color specified to GL_COLOR_BUFFER_BIT flag 
+    gluOrtho2D(-100, 100, -100, 100) #Setting X & Y coordinates for program
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1, 0, 0)
+    glPointSize(4.0)
+    glBegin(GL_POINTS)
+    glVertex2f(0, 0)
+    glEnd()
+    glFlush() #pushes the graphics specified to the screen
+
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(500, 500)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow("OpenGL Program")
+    glutDisplayFunc(draw)
+    init()
+    glutMainLoop() #To run the program continuously
+
+main()
