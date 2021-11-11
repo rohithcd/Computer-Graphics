498a00576889e5a24f5928c4f2447cda51a2bfac
Rohith Dinesan <rohithmaddz7@gmail.com>
2021-11-11 10:56:01 +0530

    updated circle1.py

--- Circle/circle1.py
+++ Circle/circle1.py
@@ -13,13 +13,14 @@ def init():
 
 def draw():
     glClear(GL_COLOR_BUFFER_BIT)
-    drawCircle()
+    MidpointCircleAlgo()
     glFlush()
 
 
-def drawCircle():
-    x = r 
-    y = 0  
+def MidpointCircleAlgo():
+    x = 0
+    y = r 
+    P = 1 - r   
     
     plot(x+xc,y+yc)
     
@@ -30,33 +31,31 @@ def drawCircle():
         plot(y+xc,x+yc)
         plot(-y+xc,x+yc)
  
-    P = 1 - r  
-  
-    while x > y: 
+    while x < y: 
       
-        y += 1
+        x += 1
  
         if P <= 0:  
-            P = P + 2 * y + 1
+            P = P + 2 * x + 1
  
         else:          
-            x -= 1
-            P = P + 2 * y - 2 * x + 1
+            y -= 1
+            P = P + 2 * x - 2 * y + 1
 
-        if (x < y): 
+        if (y < x): 
             break
  
-        plot(x+xc,y+xc)  
-        plot(-x+xc,y+xc)  
-        plot(x+xc,-y+xc)  
-        plot(-x+xc,-y+xc)  
+        plot(x+xc, y+xc)  
+        plot(-x+xc, y+xc)  
+        plot(x+xc, -y+xc)  
+        plot(-x+xc, -y+xc)  
          
 
         if x != y: 
-            plot(y+xc,x+xc)  
-            plot(-y+xc,x+xc)  
-            plot(y+xc,-x+xc)  
-            plot(-y+xc,-x+xc) 
+            plot(y+xc, x+xc)  
+            plot(-y+xc, x+xc)  
+            plot(y+xc, -x+xc)  
+            plot(-y+xc, -x+xc) 
 
 # def circle(xc, yc, r):
 
@@ -67,13 +66,16 @@ def plot(x, y):
     glBegin(GL_POINTS)
     glVertex2i(x + xc, y + yc)
     glEnd()
-
-
-def main():
+    
+    
+def getInput():
     global r, xc, yc
     r = int(input("Enter radius: "))
     xc = int(input("X Coordinate: "))
     yc = int(input("Y Coordinate: "))
+
+def main():
+    getInput()
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGB)
     glutInitWindowSize(500, 500)
